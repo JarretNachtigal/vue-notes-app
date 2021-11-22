b0a846ad857647dba8040a9af23d73a78b09dcf0
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-22 15:31:38 -0500

    selected note works

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="note-selector active">
-    <p class="note-selector-title">First note...</p>
-    <p class="note-selector-timestamp">Timestamp here...</p>
+  <div v-bind:class="{ active: selectedNoteId == note.id }" class="note-selector" v-on:click="selectNote(note)">
+    <p class="note-selector-title">{{ note.title }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note);
+    },
+  },
+};
+</script>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      :key="note.id"
+      v-on:selectNote="selectNote"
+    />
   </div>
 </template>
 
@@ -12,6 +16,20 @@ import NoteSelector from "./NoteSelector.vue";
 export default {
   components: {
     NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, title: "notes1", timestamp: Date.now() },
+        { id: 2, title: "notes2", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
